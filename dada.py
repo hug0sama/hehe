f9af14e26e2b39d414b84e0696c96173333a5a83
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-15 20:01:22 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -1,5 +1,4 @@
 # Streamlit Timeline Component Example
-
 import streamlit as st
 from streamlit_timeline import timeline
 
@@ -13,3 +12,12 @@ with open('example.json', "r") as f:
 
 # render timeline
 timeline(data, height=800)
+
+html_string = """
+            <audio controls autoplay>
+              <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
+            </audio>
+            """
+
+sound = st.empty()
+sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
