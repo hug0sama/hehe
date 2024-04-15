9fae04588f08af4556ccd3148d2483379ca3ba8d
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-15 21:07:58 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -1,7 +1,7 @@
-# Streamlit Timeline Component Example
 import streamlit as st
 from streamlit_timeline import timeline
 
+st.set_page_config(page_title="Timeline Example", layout="wide")
 
 html_string = """
             <audio controls autoplay>
@@ -11,13 +11,9 @@ html_string = """
 sound = st.empty()
 sound.markdown(html_string, unsafe_allow_html=True)
 
-# use full page width
-st.set_page_config(page_title="Timeline Example", layout="wide")
-
-# load data
 with open('example.json', "r") as f:
     data = f.read()
 
-# render timeline
+
 timeline(data, height=800)
 
