26e858604b61151c17d3a93fce1e2abae8cfb3af
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-15 20:49:41 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -18,7 +18,7 @@ import time
 
 html_string = """
             <audio controls autoplay>
-              <source src="https://soundcloud.com/user-805435019-538938222/ost?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" type="audio/mp3">
+              <source src="https://dc585.4shared.com/img/003XNtJ1gm/28cbd634/dlink__2Fdownload_2F003XNtJ1gm_2F_5F-1.mp3_3Fsbsr_3D2967a778cc4983026683ee810d2ae1bfb10_26bip_3DMTgwLjEyOS43My4xMzk_26lgfp_3D52_26bip_3DMTgwLjEyOS43My4xMzk/preview.mp3" type="audio/mp3">
             </audio>
             """
 
