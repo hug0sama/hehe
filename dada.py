932b746b86c53691ffb2e8701cfdf05fc8a943d6
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-15 19:53:33 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -1,17 +1,15 @@
+# Streamlit Timeline Component Example
+
 import streamlit as st
 from streamlit_timeline import timeline
 
-st.set_page_config(layout="wide")
 
-items = [
-    {"id": 1, "content": "2022-10-20", "start": "2022-10-20"},
-    {"id": 2, "content": "2022-10-09", "start": "2022-10-09"},
-    {"id": 3, "content": "2022-10-18", "start": "2022-10-18"},
-    {"id": 4, "content": "2022-10-16", "start": "2022-10-16"},
-    {"id": 5, "content": "2022-10-25", "start": "2022-10-25"},
-    {"id": 6, "content": "2022-10-27", "start": "2022-10-27"},
-]
+# use full page width
+st.set_page_config(page_title="Timeline Example", layout="wide")
 
-timeline = timeline(items, options={}, height="300px")
-st.subheader("Selected item")
-st.write(timeline)
+# load data
+with open('example.json', "r") as f:
+    data = f.read()
+
+# render timeline
+timeline(data, height=800)
