cf85cb2c70a3550ffaa4defd7888ab9400b0c958
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-17 11:58:48 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -3,16 +3,14 @@ from streamlit_timeline import timeline
 
 st.set_page_config(page_title="Anniversary", layout="wide")
 
-st.markdown(f'<img src="https://github.com/hug0sama/hehe/blob/main/IMG_5984.jpeg?raw=true" alt="Transparent Image" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999;" onclick="play_audio()">', unsafe_allow_html=True)
 
-def play_audio():
-    html_string = """
-            <audio controls autoplay="true">
-              <source src="https://github.com/hug0sama/hehe/blob/main/1.mp3?raw=true" type="audio/mp3">
-            </audio>
-            """
-    sound = st.empty()
-    sound.markdown(html_string, unsafe_allow_html=True)
+html_string = """
+        <audio controls autoplay="true">
+             <source src="https://github.com/hug0sama/hehe/blob/main/1.mp3?raw=true" type="audio/mp3">
+        </audio>
+        """
+sound = st.empty()
+sound.markdown(html_string, unsafe_allow_html=True)
 
 
 with open('example.json', "r") as f:
