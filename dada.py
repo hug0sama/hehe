38e962f4b849398c5c78ef6b67bdcf7cab91c09d
hug0sama <160454283+hug0sama@users.noreply.github.com>
2024-04-17 11:11:40 +0800

    Update dada.py

--- dada.py
+++ dada.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Timeline Example", layout="wide")
 
 html_string = """
             <audio controls autoplay>
-              <source src="https://dc585.4shared.com/img/003XNtJ1gm/28cbd634/dlink__2Fdownload_2F003XNtJ1gm_2F_5F-1.mp3_3Fsbsr_3D2967a778cc4983026683ee810d2ae1bfb10_26bip_3DMTgwLjEyOS43My4xMzk_26lgfp_3D52_26bip_3DMTgwLjEyOS43My4xMzk/preview.mp3" type="audio/mp3">
+              <source src="https://github.com/hug0sama/hehe/blob/main/1.mp3?raw=true" type="audio/mp3">
             </audio>
             """
 sound = st.empty()
